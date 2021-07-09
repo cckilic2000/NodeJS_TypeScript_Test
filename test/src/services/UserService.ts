1df25677cd6833f64ed04c393edc6b0245026084
Cihan Can Kılıç <cckilic2000@gmail.com>
2021-07-09 16:22:14 +0300

    service commit

--- test/src/services/UserService.ts
+++ test/src/services/UserService.ts
@@ -4,60 +4,69 @@ import userRepository from 'src/repositories/UserRepository';
 
 
 class UserService {
-
-    public async getOne(email: string): Promise<IUser> {
-        try {
-            const db = await userRepository.getOne(email);    
-            return db;
-        } catch (error) {
-            console.log("Error in UserService getOne: " + error);
-            return error;
-        }
+    public async getOne(email: string): Promise<IUser>{
+        return new Promise<IUser>( async (resolve,reject)=>{
+            await userRepository.getOne(email)
+            .then((user:IUser)=>{
+                resolve(user);
+            })
+            .catch((e:Error)=>{
+                console.log("Error in UserService getOne: " + e);
+                reject(e);
+            })
+        });
     }
 
-
-    public async getAll(): Promise<IUser[]> {
-        try {
-            const db = await userRepository.getAll();
-            return db;    
-        } catch (error) {
-            console.log("Error in UserService getAll: " + error);
-            return error;
-        }
+    public async getAll(): Promise<IUser[]>{
+        return new Promise<IUser[]>( async (resolve,reject)=>{
+            await userRepository.getAll()
+            .then((users:IUser[])=>{
+                resolve(users);
+            })
+            .catch((e:Error)=>{
+                console.log("Error in UserService getAll: " + e);
+                reject(e);
+            });
+        });
     }
 
-
-    public async add(user: IUser): Promise<number> {
-        try {
-            const done = await userRepository.add(user);
-            return done;    
-        } catch (error) {
-            console.log("Error in UserService add: " + error);
-            return error;    
-        }
+    public async add(user:IUser): Promise<number>{
+        return new Promise<number>(async (resolve,reject)=>{
+            await userRepository.add(user)
+            .then((result:number)=>{
+                resolve(result);
+            })
+            .catch((e:Error)=>{
+                console.log("Error in UserService add: " + e);
+                reject(e);
+            });
+        });
     }
 
-
-    public async update(user: IUser): Promise<number> {
-        try {
-            const db = await userRepository.update(user);
-            return db;
-        } catch (error) {
-            console.log("Error in UserService update: " + error);
-            return error;
-        }
-        
+    public async update(user:IUser):Promise<number>{
+        return new Promise<number>( async (resolve,reject)=>{
+            await userRepository.update(user)
+            .then((result:number)=>{
+                resolve(result);
+            })
+            .catch((e:Error)=>{
+                console.log("Error in UserService update: " + e);
+                reject(e);
+            });
+        });
     }
 
-
-    public async delete(id: number): Promise<number> {
-        try {
-            const db = await userRepository.delete(id);
-            return db;
-        } catch (error) {
-            console.log("Error in UserService delete: " + error);
-            return error;
-        }
+    public async delete(id: number): Promise<number>{
+        return new Promise<number>( async (resolve,reject)=>{
+            await userRepository.delete(id)
+            .then((result:number)=>{
+                resolve(result);
+            })
+            .catch((e:Error)=>{
+                console.log("Error in UserService delete: " + e);
+                reject(e);
+            });
+        })
     }
 }
 
