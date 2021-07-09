e5d7370e78e581ce20954804e17d33cc4f4aa7c8
Cihan Can Kılıç <cckilic2000@gmail.com>
2021-07-09 16:52:12 +0300

    validation max min

--- test/src/validations/UserValidation.ts
+++ test/src/validations/UserValidation.ts
@@ -3,12 +3,12 @@ import Joi from 'joi';
 
 export const userSchema = {
     add: Joi.object().keys({
-        name: Joi.string().required(),
+        name: Joi.string().min(3).max(30).required(),
         email: Joi.string().email().required(),
         id: Joi.number()
     }),
     update: Joi.object().keys({
-        name: Joi.string().required(),
+        name: Joi.string().min(3).max(30).required(),
         email: Joi.string().email().required(),
         id: Joi.number()
     }),
